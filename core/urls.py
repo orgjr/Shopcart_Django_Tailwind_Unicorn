532872192b96b9efd9ebcb5ce9e80fdd69ec2cc3
orgjr <orgj@protonmail.com>
2026-06-16 14:08:25 -0300

    chore: stop tracking ignored files

--- core/urls.py
+++ core/urls.py
@@ -1,31 +1,12 @@
-from django.urls import path
-from core import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+from core import views
 
 urlpatterns = [
-    path(
-        "",
-        views.index,
-        name="index",
-    ),
-    path(
-        "login/",
-        LoginView.as_view(template_name="core/login.html"),
-        name="login",
-    ),
-    path(
-        "shop/cart/",
-        views.cart,
-        name="cart",
-    ),
-    path(
-        "logout/",
-        LogoutView.as_view(),
-        name="logout",
-    ),
-    path(
-        "you/left/the/system/",
-        views.logged_out,
-        name="logged_out",
-    ),
+    path("", views.index, name="index"),
+    path("login/", LoginView.as_view(template_name="core/login.html"), name="login"),
+    path("shop/cart/", views.cart, name="cart"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("you/left/the/system/", views.logged_out, name="logged_out"),
 ]
