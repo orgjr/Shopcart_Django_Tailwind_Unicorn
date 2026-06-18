c88749ae15ae4f4832f32b33e4917b09e1100a21
orgjr <orgj@protonmail.com>
2026-06-18 13:51:55 -0300

    fix: add fallback for already registered instances

--- core/management/commands/populate.py
+++ core/management/commands/populate.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
+from django.db import IntegrityError
 
 from core.models import Product
 
@@ -8,17 +9,20 @@ class Command(BaseCommand):
     help = "Load product data to the database"
 
     def handle(self, *args, **kwargs):
-        Product.objects.get_or_create(name="Figo", price=18.50)
+        try:
+            Product.objects.get_or_create(name="Figo", price=18.50)
 
-        Product.objects.get_or_create(name="Lazanha", price=8.50)
+            Product.objects.get_or_create(name="Lazanha", price=8.50)
 
-        Product.objects.get_or_create(name="TV", price=1880.49)
+            Product.objects.get_or_create(name="TV", price=1880.49)
 
-        Product.objects.get_or_create(name="Air Frier", price=455.69)
+            Product.objects.get_or_create(name="Air Frier", price=455.69)
 
-        Product.objects.get_or_create(name="Algodão", price=3.50)
+            Product.objects.get_or_create(name="Algodão", price=3.50)
 
-        Product.objects.get_or_create(name="Óleo de soja", price=8.19)
+            Product.objects.get_or_create(name="Óleo de soja", price=8.19)
 
-        User.objects.create_user(username="user", password="123")
-        User.objects.create_superuser(username="adm", password="123")
+            User.objects.create_user(username="user", password="123")
+            User.objects.create_superuser(username="adm", password="123")
+        except IntegrityError:
+            return
